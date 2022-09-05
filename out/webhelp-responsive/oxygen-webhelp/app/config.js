8aa67ff7a7b30a5845497150b0d3a2ba7c0a134b
Yves Barbion <yves.athayoga@gmail.com>
2022-09-05 11:21:38 +0200

    .

--- out/webhelp-responsive/oxygen-webhelp/app/config.js
+++ out/webhelp-responsive/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220905105004';
+                args = '20220905112059';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {5:-1,4:5,0:-1,3:-1,1:-1,6:5};
+return {4:5,0:-1,3:-1,1:-1,6:5,5:-1};
 });
