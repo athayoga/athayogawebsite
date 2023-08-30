740a987fc06a52ed4ee69f3010a2187b87607c14
athayoga <yves.barbion@flowtime.be>
2023-08-30 18:06:31 +0200

    z

--- out/webhelp-responsive/oxygen-webhelp/app/config.js
+++ out/webhelp-responsive/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230830172117';
+                args = '20230830180406';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"Bio",p:["p0"]},{w:"Contact",p:["p1"]},{w:"Sankalpa",p:["p2"]},{w:"Atha",p:["p3"]},{w:"yoga?",p:["p3"]},{w:"Hatha",p:["p3"]},{w:"yoga",p:["p3"]},{w:"Kine",p:["p4"]},{w:"Consult",p:["p4"]},{w:"(Gent)",p:["p4","p7"]},{w:"Lessen",p:["p5"]},{w:"Polikliniek",p:["p6"]},{w:"Rode",p:["p6"]},{w:"Heuvel",p:["p6"]},{w:"Studio",p:["p7"]},{w:"Groene",p:["p7"]},{w:"Vallei",p:["p7"]},{w:"Warande",p:["p8"]},{w:"(Wetteren)",p:["p8"]}];
+define(function() {var keywords=[{w:"Bio",p:["p0"]},{w:"Contact",p:["p1"]},{w:"Sankalpa",p:["p2"]},{w:"Atha",p:["p3"]},{w:"yoga?",p:["p3"]},{w:"Hatha",p:["p3"]},{w:"yoga",p:["p3"]},{w:"Kine",p:["p4"]},{w:"Consult",p:["p4"]},{w:"(Gent)",p:["p4","p7"]},{w:"Lessen",p:["p5"]},{w:"Polikliniek",p:["p6"]},{w:"Rode",p:["p6"]},{w:"Heuvel",p:["p6"]},{w:"(Wetteren)",p:["p6","p8"]},{w:"Studio",p:["p7"]},{w:"Groene",p:["p7"]},{w:"Vallei",p:["p7"]},{w:"Warande",p:["p8"]}];
 var ph={};
 ph["p0"]=[0];
 ph["p1"]=[1];
@@ -6,9 +6,9 @@ ph["p2"]=[2];
 ph["p3"]=[3, 4, 5, 6];
 ph["p4"]=[7, 8, 9];
 ph["p5"]=[10];
-ph["p6"]=[11, 12, 13];
-ph["p7"]=[14, 15, 16, 9];
-ph["p8"]=[17, 18];
+ph["p6"]=[11, 12, 13, 14];
+ph["p7"]=[15, 16, 17, 9];
+ph["p8"]=[18, 14];
      return {
          keywords: keywords,
          ph: ph
