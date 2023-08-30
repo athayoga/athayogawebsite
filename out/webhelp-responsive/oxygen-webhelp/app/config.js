fdd48330710c31d692fefc83716b3ff2fe4b7cad
athayoga <yves.barbion@flowtime.be>
2023-08-30 18:11:52 +0200

    Update config.js

--- out/webhelp-responsive/oxygen-webhelp/app/config.js
+++ out/webhelp-responsive/oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230830180838';
+                args = '20230830181124';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
